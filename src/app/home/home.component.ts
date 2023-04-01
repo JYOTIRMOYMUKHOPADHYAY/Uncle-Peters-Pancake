d33d22415f944a3a81c1a0c7b06895a572e0f155
JYOTIRMOY MUKHOPADHYAY <mukhopadhyayjyotirmoy2@gmail.com>
2023-04-01 14:41:33 +0530

    dummy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+// amCharts imports
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
+import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,4 +12,11 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 })
 export class HomeComponent {
   faCoffee = faCoffee;
+  root = am5.Root.new("chartdiv"); 
+  chart = this.root.container.children.push(
+  am5map.MapChart.new(this.root, {
+    panX: "rotateX",
+    projection: am5map.geoNaturalEarth1()
+  })
+);
 }
